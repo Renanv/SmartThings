4635d00b7f1d133a58c71ca460b6b0c5fbfdc62c
ziki <zikria.azimi@hotmail.com>
2019-09-10 11:29:23 +0200

    index.html toegevoegd

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>login</title>
+</head>
+<body>
+    <form action="log.php" method="post">
+        <label>Voer hier je naam in</label>
+        <input type="text" name="username">
+        <label>Voer hier je wachtwoord in</label>
+        <input type="password" name="password" />
+        <input type="submit" value="inloggen">
+    </form>
+</body>
+</html>
