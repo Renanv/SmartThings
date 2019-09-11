7a7e72d394f5d1fbacdbb05dd51cd53b82650fa4
ziki <zikria.azimi@hotmail.com>
2019-09-11 11:29:43 +0200

    better now

--- login.php
+++ login.php
@@ -1,87 +1,68 @@
 <?php
-// Initialize the session
 session_start();
 
-// Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: log.php");
     exit;
 }
 
-// Include config file
 require_once "config.php";
 
-// Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
 
-// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Check if username is empty
     if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
+        $username_err = "Vul AUB je gebruikersnaam in.";
     } else{
         $username = trim($_POST["username"]);
     }
 
-    // Check if password is empty
     if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
+        $password_err = "Vul AUB je wachtwoord in.";
     } else{
         $password = trim($_POST["password"]);
     }
 
-    // Validate credentials
     if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = :username";
 
         if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
 
-            // Set parameters
             $param_username = trim($_POST["username"]);
 
-            // Attempt to execute the prepared statement
             if($stmt->execute()){
-                // Check if username exists, if yes then verify password
                 if($stmt->rowCount() == 1){
                     if($row = $stmt->fetch()){
                         $id = $row["id"];
                         $username = $row["username"];
                         $hashed_password = $row["password"];
                         if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
                             session_start();
 
-                            // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
 
-                            // Redirect user to welcome page
                             header("location: log.php");
                         } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
+                            $password_err = "Je wachtwoord is verkeerd ahaahah";
                         }
                     }
                 } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
+                    $username_err = "Geen account gevonden met deze gebruikersnaam.";
                 }
             } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                echo "Iets is verkeerd gegaan loolllllll.";
             }
         }
 
-        // Close statement
         unset($stmt);
     }
 
-    // Close connection
     unset($pdo);
 }
 ?>
@@ -90,7 +71,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <title>Inloggen</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; }
@@ -100,7 +81,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
 <div class="wrapper">
     <h2>Login</h2>
-    <p>Please fill in your credentials to login.</p>
+    <p>Vul je inloggegevens in.</p>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
             <label>Username</label>
@@ -108,14 +89,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <span class="help-block"><?php echo $username_err; ?></span>
         </div>
         <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-            <label>Password</label>
+            <label>Wachtwoord</label>
             <input type="password" name="password" class="form-control">
             <span class="help-block"><?php echo $password_err; ?></span>
         </div>
         <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Login">
+            <input type="submit" class="btn btn-primary" value="Inloggen">
         </div>
-        <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        <p>Heb je geen account? <a href="register.php">Registreer nu</a>.</p>
     </form>
 </div>
 </body>
